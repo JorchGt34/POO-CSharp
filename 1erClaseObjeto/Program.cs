b5e8f8727a8b9be9146cddb523ddb3f8b6895870
Jorch34 <jorchgt34@gmail.com>
2025-03-20 11:43:20 -0600

    Estructura de una clase

---
+++ 1erClaseObjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿//Una clase es un molde para crear objetos
+ControlXboxElite controlJorge = new ControlXboxElite();
+ControlXboxElite controlNatalia = new ControlXboxElite();
+
+controlJorge.Color = "Negro";
+controlNatalia.Color = "Azul";
+class ControlXboxElite
+{
+    public string Color;
+    public double Largo;
+    public short NumeroDeBotones;
+    public bool TieneAccesoriosPuestos;
+}
