1c823338e66b8b071653ca267d2c28aa72063299
Jorch34 <jorchgt34@gmail.com>
2025-03-21 10:08:55 -0600

    Cracion de clases, objetos, listas, listas de objetos, agregar objetos a listas

--- 1erClaseObjeto/Program.cs
+++ 1erClaseObjeto/Program.cs
@@ -4,10 +4,28 @@ ControlXboxElite controlNatalia = new ControlXboxElite();
 
 controlJorge.Color = "Negro";
 controlNatalia.Color = "Azul";
+
+SuperHeroe sonic = new SuperHeroe();
+sonic.nombre = "Sonic The Hedgehog";
+sonic.superPoder = "Super velocidad";
+sonic.estaturaCm = 90;
+sonic.edad = 15;
+sonic.tieneIdentidadSecreta = false;
+sonic.usaCapa = false;
+Console.WriteLine(sonic.tieneIdentidadSecreta);
 class ControlXboxElite
 {
     public string Color;
     public double Largo;
     public short NumeroDeBotones;
     public bool TieneAccesoriosPuestos;
+}
+
+class SuperHeroe {
+    public string nombre;
+    public string superPoder;
+    public int estaturaCm;
+    public int edad;
+    public bool tieneIdentidadSecreta;
+    public bool usaCapa;
 }
---
+++ SuperHeroesApp/Program.cs
@@ -0,0 +1,69 @@
+﻿//Poderes
+var poderVolar = new SuperPoder();
+
+poderVolar.Nombre = "Volar";
+poderVolar.Descripcion = "Capacidad para volar y planear en el aire a alta velocidad";
+poderVolar.Nivel = NivelPoder.NivelDos;
+
+var superFuerza = new SuperPoder();
+
+superFuerza.Nombre = "Super fuerza";
+superFuerza.Descripcion = "Capacidad para levantar objetos pesados y realizar proezas físicas sobrehumanas";
+superFuerza.Nivel = NivelPoder.NivelTres;
+
+var superVelocidad = new SuperPoder();
+
+superVelocidad.Nombre = "Super velocidad";
+superVelocidad.Descripcion = "Capacidad para moverse a velocidades extremadamente altas";
+superVelocidad.Nivel = NivelPoder.NivelTres;
+
+//Lista de superpoderes
+List<SuperPoder> poderesSuperman = new List<SuperPoder>();
+poderesSuperman.Add(poderVolar);
+poderesSuperman.Add(superFuerza);
+
+List<SuperPoder> poderesFlash = new List<SuperPoder>();
+poderesFlash.Add(superFuerza);
+poderesFlash.Add(superVelocidad);
+
+//Super heroes
+var superman = new SuperHeroe();
+
+superman.Id = 1;
+superman.Nombre = "Superman";
+superman.IdentidadSecreta = "Clark Kent";
+superman.Ciudad = "Metropolis";
+superman.PuedeVolar = true;
+superman.SuperPoderes = poderesSuperman;
+
+var flash = new SuperHeroe();
+
+flash.Id = 2;
+flash.Nombre = "The Flash";
+flash.IdentidadSecreta = "Barry Allen";
+flash.Ciudad = "Central City";
+flash.PuedeVolar = false;
+flash.SuperPoderes = poderesFlash;
+
+Console.WriteLine(flash.SuperPoderes[0].Descripcion);
+class SuperHeroe 
+{
+    public int Id;
+    public string Nombre;
+    public string IdentidadSecreta;
+    public string Ciudad;
+    public List<SuperPoder> SuperPoderes;
+    public bool PuedeVolar;
+}
+class SuperPoder 
+{
+    public string Nombre;
+    public string Descripcion;
+    public NivelPoder Nivel;
+}
+enum NivelPoder
+{
+    NivelUno,
+    NivelDos,
+    NivelTres
+}
